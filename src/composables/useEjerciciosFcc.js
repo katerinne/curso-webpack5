83514ab26f9708b7c83a46feca16edfb953e43f3
clustersnake <clustersnake45@gmail.com>
2021-06-30 23:09:36 -0400

    feat: Crear archivos base para realizar los ejercicios de freecodecamp

---
+++ src/composables/useEjerciciosFcc.js
@@ -0,0 +1,11 @@
+const useEjerciciosFcc = () => {
+  let mensaje = "mensaje inicial";
+
+  const setMensaje = (msj) => (mensaje = msj);
+  const getMensaje = () => mensaje;
+
+  return {
+    setMensaje,
+    getMensaje,
+  };
+};
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const main = () => {
+  const { setMensaje, getMensaje } = useEjerciciosFcc();
+
+  console.group("hola mundo");
+  console.log("antes de asignar el valor: hola mundo ->", getMensaje());
+  setMensaje("hola mundo");
+  console.log("después de asignar el valor: hola mundo ->", getMensaje());
+  console.groupEnd();
+};
+
+main();
